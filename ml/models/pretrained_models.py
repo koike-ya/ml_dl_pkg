5c7600bb581b4ad4d568449a26a2731fbcec0d05
Tomoya Koike <makeffort134@gmail.com>
2019-12-24 16:45:58 +0900

    pretrain added

--- ml/models/pretrained_models.py
+++ ml/models/pretrained_models.py
@@ -11,17 +11,28 @@ import os
 import copy
 
 
-supported_pretrained_models = {'resnet18': models.resnet18}
+supported_pretrained_models = {'resnet18': models.resnet18, 'alexnet': models.alexnet, 'densenet': models.densenet121,
+                               'wideresnet': models.wide_resnet50_2, 'inception_v3': models.inception_v3,
+                               'resnext': models.resnext50_32x4d, 'vgg19': models.vgg19, 'googlenet': models.googlenet,
+                               }
 
 
 def construct_pretrained(cfg, n_classes):
     model = supported_pretrained_models[cfg['model_type']](pretrained=True)
-    print(model)
-    num_ftrs = model.fc.in_features
+    if cfg['model_type'] in ['resnet18']:
+        num_ftrs = model.fc.in_features
 
-    model.fc = nn.Sequential(
-        nn.Linear(num_ftrs, n_classes),
-        nn.Softmax(dim=1),
-    )
+        model.fc = nn.Sequential(
+            nn.Linear(num_ftrs, n_classes),
+            nn.Softmax(dim=1),
+        )
+    elif cfg['model_type'] in ['alexnet']:
+        num_ftrs = model.classifier[6].in_features
+
+        model.classifier[6] = nn.Sequential(
+            nn.Linear(num_ftrs, n_classes),
+            nn.Softmax(dim=1),
+        )
+
 
     return model
