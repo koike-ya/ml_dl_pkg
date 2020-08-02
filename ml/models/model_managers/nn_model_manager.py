[fix] apex mix precision function is fixed

ml/models/model_managers/nn_model_manager.py
@@ -28,9 +28,9 @@ from ml.utils.nn_config import SGDConfig, AdamConfig
 class NNModelManager(BaseModelManager):
     def __init__(self, class_labels, cfg):
         super().__init__(class_labels, cfg)
-        self.device = torch.device('cuda' if cfg['cuda'] else 'cpu')
-        self.model = self._init_model(transfer=cfg['transfer']).to(self.device)
-        self.mixup_alpha = cfg['mixup_alpha']
+        self.device = torch.device('cuda' if cfg.cuda else 'cpu')
+        self.model = self._init_model(transfer=cfg.transfer).to(self.device)
+        self.mixup_alpha = cfg.mixup_alpha
         if self.mixup_alpha:
             self._orig_criterion = self.criterion.to(self.device)
             self.criterion = self._mixup_criterion(lamb=1.0)
@@ -38,33 +38,33 @@ class NNModelManager(BaseModelManager):
             self.criterion = self.criterion.to(self.device)
         self.optimizer = self._set_optimizer()
         self.fitted = False
-        self.amp = cfg.get('amp', False)
+        self.amp = cfg.amp
         if self.amp:
             self.model, self.optimizer = amp.initialize(self.model, self.optimizer)
-        if torch.cuda.device_count() > 1 and cfg['model_type'] not in ['rnn', 'cnn_rnn']:
+        if torch.cuda.device_count() > 1 and cfg.model_type not in ['rnn', 'cnn_rnn']:
             self.model = torch.nn.DataParallel(self.model)
 
     def _init_model(self, transfer=False):
         if transfer:
             orig_classes = self.class_labels
-            self.class_labels = self.cfg['prev_classes']
-        if self.cfg['model_type'].value in supported_pretrained_models.keys():
+            self.class_labels = self.cfg.prev_classes
+        if self.cfg.model_type.value in supported_pretrained_models.keys():
             model = construct_pretrained(self.cfg, len(self.class_labels))
-        elif self.cfg['model_type'].value == 'nn':
+        elif self.cfg.model_type.value == 'nn':
             model = construct_nn(self.cfg)
-        elif self.cfg['model_type'].value == 'rnn':
+        elif self.cfg.model_type.value == 'rnn':
             model = construct_rnn(self.cfg, len(self.class_labels))
-        elif self.cfg['model_type'].value == 'cnn_rnn':
+        elif self.cfg.model_type.value == 'cnn_rnn':
             model = construct_cnn_rnn(self.cfg, construct_cnn, len(self.class_labels), self.device)
-        elif self.cfg['model_type'].value == 'cnn':
+        elif self.cfg.model_type.value == 'cnn':
             model = construct_cnn(self.cfg, use_as_extractor=False)
-        elif self.cfg['model_type'].value == 'logmel_cnn':
+        elif self.cfg.model_type.value == 'logmel_cnn':
             model = construct_logmel_cnn(self.cfg)
-        elif self.cfg['model_type'].value == 'panns':
+        elif self.cfg.model_type.value == 'panns':
             model = construct_panns(self.cfg)
-        elif self.cfg['model_type'].value == 'attention_cnn':
+        elif self.cfg.model_type.value == 'attention_cnn':
             model = construct_attention_cnn(self.cfg)
-        elif self.cfg['model_type'].value == 'multitask_panns':
+        elif self.cfg.model_type.value == 'multitask_panns':
             model = construct_multitask_panns(self.cfg)
         else:
             raise NotImplementedError('model_type should be either rnn or cnn, nn would be implemented in the future.')
@@ -184,7 +184,7 @@ class NNModelManager(BaseModelManager):
     def fit(self, inputs, labels, phase) -> Tuple[float, np.ndarray]:
         self.fitted = True
         self.optimizer.zero_grad()
-        if self.cfg['task_type'].value == 'classify':
+        if self.cfg.task_type.value == 'classify':
             return self._fit_classify(inputs, labels, phase)
         else:
             return self._fit_regress(inputs, labels, phase)
@@ -197,12 +197,12 @@ class NNModelManager(BaseModelManager):
             self.model = model
 
         try:
-            self.model.load_state_dict(torch.load(self.cfg['model_path'], map_location=self.device))
+            self.model.load_state_dict(torch.load(self.cfg.model_path, map_location=self.device))
             self.model.to(self.device)
             logger.info('Saved model loaded.')
         except FileNotFoundError as e:
             logger.info(e)
-            logger.info(f"trained model file doesn't exist at {self.cfg['model_path']}")
+            logger.info(f"trained model file doesn't exist at {self.cfg.model_path}")
             exit(1)
 
         self.fitted = True
@@ -215,7 +215,7 @@ class NNModelManager(BaseModelManager):
             self.model.eval()
             preds = self.model(inputs)
 
-            if self.cfg['task_type'].value == 'classify':
+            if self.cfg.task_type.value == 'classify':
                 if hasattr(self, 'predictor'):
                     # TODO classifierも別ファイルに重みを保存しておいて、train_managerで読み込み
                     preds = torch.from_numpy(self.predictor.predict(preds.detach()))
@@ -227,5 +227,5 @@ class NNModelManager(BaseModelManager):
 
     def update_by_epoch(self, phase):
         for g in self.optimizer.param_groups:
-            g['lr'] = g['lr'] / self.cfg['learning_anneal']
+            g['lr'] = g['lr'] / self.cfg.learning_anneal
         logger.info(f"Learning rate annealed to: {g['lr']:.6f}")